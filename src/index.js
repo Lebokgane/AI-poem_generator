827aa309376b3366f8394fd804c4cd79b7305f5d
Lebokgane <lebokgane1@gmail.com>
2025-11-27 22:36:03 +0200

    Added API, Prompt, and context

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-    let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
+    console.log("poem generated");
     new Typewriter('#poem', {
-    strings: "La tombe dit a` la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
-    cursor: ""
+    cursor: "",
 });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "efab59oa8043330da286420bt3f32b43";
+    let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions. Sign the poem with `SheCodes AI` in a strong element at the bottom of the poem";
+    let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Generating poem");
+    console.log(`prompt: ${prompt}`);
+    console.log(`context: ${context}`);
+
+    axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
